8977e7407e1ad1c03ae9edb1f97068b250bbeb8f
Prathameshcode <72982933+Prathameshcode@users.noreply.github.com>
2020-11-25 20:15:06 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,45 @@
+class Chain {
+
+    constructor(bodyA, pointB) {
+
+        var options={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness: 0.2,
+            length: 30
+        }
+
+        this.pointB = pointB;
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+
+    }
+
+    fly() {
+        this.chain.bodyA = null;
+    }
+
+    attach(body) {
+        this.chain.bodyA = body;
+    }
+
+    display() {
+
+        if(this.chain.bodyA) {
+
+            var pointA = this.chain.bodyA.position;
+            var pointB = this.pointB;
+
+            push();
+            stroke("maroon");
+            strokeWeight(5);
+            fill("blue");
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            pop();
+
+        }
+
+    }
+    
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# kingcoder
+# p5.play-boilerplate
+Boiler plate for p5.play
